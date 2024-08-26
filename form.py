b132be6c8d85bd543118fc916e218bb94eea041d
MChandraR <2201020103@student.umrah.ac.id>
2024-08-26 12:54:24 +0700

    Update Source Code and UI

---
+++ form.py
@@ -0,0 +1,162 @@
+import tkinter as tk
+from tkinter import  ttk
+from PIL import ImageTk, Image
+from tkinter import StringVar
+import time
+from threading import Thread
+
+import cv2
+
+class VideoMonitorApp(tk.Tk):
+    def __init__(self, mqtt):
+        super().__init__()
+        self.title("Video Monitor App")
+        self.geometry("1230x640")
+        self.mqtt = mqtt
+        
+        # Bagian baris pertama untuk video monitoring
+        self.video_frame_1 = tk.Label(self, text="Video 1", bg="gray", width=55, height=20)
+        self.video_frame_2 = tk.Label(self, text="Video 2", bg="gray", width=55, height=20)
+        self.video_frame_3 = tk.Label(self, text="Video 3", bg="gray", width=55, height=20)
+        
+        self.video_frame_1.grid(row=0, column=0, padx=10, pady=10)
+        self.video_frame_2.grid(row=0, column=1, padx=10, pady=10)
+        self.video_frame_3.grid(row=0, column=2, padx=10, pady=10)
+        
+        # Baris kedua, kolom pertama untuk detail koordinat dan informasi
+        self.detail_frame = tk.Frame(self, bg="white", width=10, height=10)
+        self.detail_frame.grid(row=1, column=0, columnspan=2, padx=10, pady=10, sticky="nsew")
+        
+        # Data 1
+        self.coordinate_label = tk.Label(self.detail_frame, text="Azimuth:")
+        self.coordinate_label.grid(row=0, column=0, padx=5, pady=5, sticky="w")
+        
+        self.coordinate_value = StringVar()
+        self.coordinate_value.set("(X: 0, Y: 0)")
+        self.coordinate_value_label = tk.Label(self.detail_frame, textvariable=self.coordinate_value)
+        self.coordinate_value_label.grid(row=0, column=1, padx=5, pady=5, sticky="w")
+        
+        #Data 2
+        self.counter_label = tk.Label(self.detail_frame, text="Counter:")
+        self.counter_label.grid(row=1, column=0, padx=5, pady=5, sticky="w")
+        
+        self.counter_value = StringVar()
+        self.counter_value.set("0")
+        self.counter_value_label = tk.Label(self.detail_frame, textvariable=self.counter_value)
+        self.counter_value_label.grid(row=1, column=1, padx=5, pady=5, sticky="w")
+        
+        #Data 3
+        self.counter_label = tk.Label(self.detail_frame, text="Lat:")
+        self.counter_label.grid(row=2, column=0, padx=5, pady=5, sticky="w")
+        
+        self.counter_value = StringVar()
+        self.counter_value.set("0")
+        self.counter_value_label = tk.Label(self.detail_frame, textvariable=self.counter_value)
+        self.counter_value_label.grid(row=2, column=1, padx=5, pady=5, sticky="w")
+        
+        #Data 4
+        self.counter_label = tk.Label(self.detail_frame, text="Long:")
+        self.counter_label.grid(row=3, column=0, padx=5, pady=5, sticky="w")
+        
+        self.counter_value = StringVar()
+        self.counter_value.set("0")
+        self.counter_value_label = tk.Label(self.detail_frame, textvariable=self.counter_value)
+        self.counter_value_label.grid(row=3, column=1, padx=5, pady=5, sticky="w")
+        
+        #Data 5
+        self.counter_label = tk.Label(self.detail_frame, text="Lat Direction:")
+        self.counter_label.grid(row=4, column=0, padx=5, pady=5, sticky="w")
+        
+        self.counter_value = StringVar()
+        self.counter_value.set("0")
+        self.counter_value_label = tk.Label(self.detail_frame, textvariable=self.counter_value)
+        self.counter_value_label.grid(row=4, column=1, padx=5, pady=5, sticky="w")
+        
+        #Data 6
+        self.counter_label = tk.Label(self.detail_frame, text="Long Direction:")
+        self.counter_label.grid(row=5, column=0, padx=5, pady=5, sticky="w")
+        
+        self.counter_value = StringVar()
+        self.counter_value.set("0")
+        self.counter_value_label = tk.Label(self.detail_frame, textvariable=self.counter_value)
+        self.counter_value_label.grid(row=5, column=1, padx=5, pady=5, sticky="w")
+        
+        
+        # Baris kedua, kolom kedua untuk tombol pengaturan counter dan posisi
+        self.control_frame = tk.Frame(self)
+        self.control_frame.grid(row=1, column=2, padx=10, pady=10, sticky="nsew")
+        
+        self.update_counter_button = tk.Button(self.control_frame, text="Counter +", command=self.update_counter_plus)
+        self.update_counter_button.grid(row=0, column=0, padx=5, pady=5)
+        
+        self.update_position_button = tk.Button(self.control_frame, text="Counter -", command=self.update_counter_min)
+        self.update_position_button.grid(row=1, column=0, padx=5, pady=5)
+        
+        self.counter_entry = tk.Entry(self.control_frame)
+        self.counter_entry.grid(row=0, column=3, padx=5, pady=5)
+        
+        self.coor_entry = tk.Entry(self.control_frame)
+        self.coor_entry.grid(row=0, column=4, padx=5, pady=5)
+        
+        #tambahan
+        self.detail_frames = tk.Frame(self, bg="white", width=10, height=10)
+        self.detail_frames.grid(row=2, column=0, padx=10, pady=10, sticky="nsew")
+        self.log_label = tk.Label(self.detail_frames, text="Log:")
+        self.log_label.grid(row=3, column=0, padx=5, pady=5, sticky="w")
+
+        # Counter dan posisi default
+        self.counter = 0
+        self.x_position = 0
+        self.y_position = 0
+        
+        # Simulasi video update
+        self.after(1000, self.update_video_frames)
+
+    def update_video_frames(self):
+        # Simulasi update frame video setiap detik
+        current_time = time.strftime('%H:%M:%S')
+        self.video_frame_1.config(text=f"Video 1 - {current_time}")
+        self.video_frame_2.config(text=f"Video 2 - {current_time}")
+        self.video_frame_3.config(text=f"Video 3 - {current_time}")
+        self.after(1000, self.update_video_frames)  # Update setiap 1 detik
+    
+    def display_frame(self, label, frame):
+        # Konversi frame BGR OpenCV ke RGB dan menampilkan di Tkinter Label
+        frame = cv2.resize(frame , (400, 300))
+        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        img = Image.fromarray(frame_rgb)
+        imgtk = ImageTk.PhotoImage(image=img)
+        label.config(image=imgtk, width=400, height = 300)
+        label.imgtk = imgtk
+    
+    
+    def update_counter_plus(self):
+        self.mqtt.counter +=1
+        self.counter_value.set(str(self.mqtt.counter))
+        
+    def update_counter_min(self):
+        self.mqtt.counter -=1
+        self.counter_value.set(str(self.mqtt.counter))
+
+    def update_position(self):
+        self.x_position += 10
+        self.y_position += 5
+        self.coordinate_value.set(f"(X: {self.x_position}, Y: {self.y_position})")
+
+    def update_log(self, msg):
+        self.log_label.set(str(msg))
+
+        
+    def on_close(self):
+        # Fungsi untuk menutup aplikasi dengan benar dan melepaskan resource
+       
+        self.destroy()
+
+def launchApp(mqtt):
+    global app
+    app = VideoMonitorApp(mqtt)
+    app.protocol("WM_DELETE_WINDOW", app.on_close)
+    app.mainloop()
+
+
+launchApp(None)
--- server.py
+++ server.py
@@ -12,6 +12,7 @@ import numpy as np
 from threading import Thread
 import mqtt_test
 
+import form 
 import socketio
 import asyncio
 import pathlib
@@ -83,6 +84,7 @@ model = DetectMultiBackend(weights=weights, device=device, dnn=False, data=data,
     
 @smart_inference_mode()
 async def run(
+    idx,
     weights=ROOT / "exp5/weights/best.pt",  # model path or triton URL
     source="http://192.168.2.148:8081/?action=stream",  # file/dir/URL/glob/screen/0(webcam)
     data=ROOT / "datasets/data.yaml",  # dataset.yaml path
@@ -208,12 +210,21 @@ async def run(
 
             # Stream results
             im0 = annotator.result()
+            frames = None
             if view_img:
-                if platform.system() == "Linux" and p not in windows:
-                    windows.append(p)
-                    cv2.namedWindow(str(p), cv2.WINDOW_NORMAL | cv2.WINDOW_KEEPRATIO)  # allow window resize (Linux)
-                    cv2.resizeWindow(str(p), im0.shape[1], im0.shape[0])
-                cv2.imshow(str(p), im0)
+                # if platform.system() == "Linux" and p not in windows:
+                #     windows.append(p)
+                #     cv2.namedWindow(str(p), cv2.WINDOW_NORMAL | cv2.WINDOW_KEEPRATIO)  # allow window resize (Linux)
+                #     cv2.resizeWindow(str(p), im0.shape[1], im0.shape[0])
+                # cv2.imshow(str(p), im0)
+                if idx == 1:
+                    frames = form.app.video_frame_1
+                elif idx == 2:
+                    frames = form.app.video_frame_2
+                elif idx == 3:
+                    frames = form.app.video_frame_3
+
+                form.app.display_frame(frames,im0)
                 cv2.waitKey(1)  # 1 millisecond
 
             # Save results (image with detections)
@@ -246,171 +257,6 @@ async def run(
         LOGGER.info(f"Results saved to {colorstr('bold', save_dir)}{s}")
     if update:
         strip_optimizer(weights[0])  # update model (to fix SourceChangeWarning)
-        
-async def run2(
-    weights=ROOT / "yolov5s.pt",  # model path or triton URL
-    source="http://192.168.2.148:8080/?action=stream",  # file/dir/URL/glob/screen/0(webcam)
-    data=ROOT / "data/coco128.yaml",  # dataset.yaml path
-    imgsz=(640, 640),  # inference size (height, width)
-    conf_thres=0.25,  # confidence threshold
-    iou_thres=0.45,  # NMS IOU threshold
-    max_det=1000,  # maximum detections per image
-    device="",  # cuda device, i.e. 0 or 0,1,2,3 or cpu
-    view_img=False,  # show results
-    save_txt=False,  # save results to *.txt
-    save_csv=False,  # save results in CSV format
-    save_conf=False,  # save confidences in --save-txt labels
-    save_crop=False,  # save cropped prediction boxes
-    nosave=False,  # do not save images/videos
-    classes=None,  # filter by class: --class 0, or --class 0 2 3
-    agnostic_nms=False,  # class-agnostic NMS
-    augment=False,  # augmented inference
-    visualize=False,  # visualize features
-    update=False,  # update all models
-    project=ROOT / "runs/detect",  # save results to project/name
-    name="exp",  # save results to project/name
-    exist_ok=False,  # existing project/name ok, do not increment
-    line_thickness=3,  # bounding box thickness (pixels)
-    hide_labels=False,  # hide labels
-    hide_conf=False,  # hide confidences
-    half=False,  # use FP16 half-precision inference
-    dnn=False,  # use OpenCV DNN for ONNX inference
-    vid_stride=1,  # video frame-rate stride
-):
-    global model
-    # Directories
-    save_dir = increment_path(Path(project) / name, exist_ok=exist_ok)  # increment run
-    (save_dir / "labels" if save_txt else save_dir).mkdir(parents=True, exist_ok=True)  # make dir
-    # Load model
-   
-    stride, names, pt = model.stride, model.names, model.pt
-    imgsz = check_img_size(imgsz, s=stride)  # check image size
-
-    # Dataloader
-    bs = 1  # batch_size
- 
-    view_img = check_imshow(warn=True)
-    dataset = LoadStreams(source, img_size=imgsz, stride=stride, auto=pt, vid_stride=vid_stride)
-    bs = len(dataset)
-
-    vid_path, vid_writer = [None] * bs, [None] * bs
-
-    # Run inference
-    model.warmup(imgsz=(1 if pt or model.triton else bs, 3, *imgsz))  # warmup
-    seen, windows, dt = 0, [], (Profile(device=device), Profile(device=device), Profile(device=device))
-    for path, im, im0s, vid_cap, s in dataset:
-        with dt[0]:
-            im = torch.from_numpy(im).to(model.device)
-            im = im.half() if model.fp16 else im.float()  # uint8 to fp16/32
-            im /= 255  # 0 - 255 to 0.0 - 1.0
-            if len(im.shape) == 3:
-                im = im[None]  # expand for batch dim
-            if model.xml and im.shape[0] > 1:
-                ims = torch.chunk(im, im.shape[0], 0)
-
-        # Inference
-        with dt[1]:
-            visualize = increment_path(save_dir / Path(path).stem, mkdir=True) if visualize else False
-            if model.xml and im.shape[0] > 1:
-                pred = None
-                for image in ims:
-                    if pred is None:
-                        pred = model(image, augment=augment, visualize=visualize).unsqueeze(0)
-                    else:
-                        pred = torch.cat((pred, model(image, augment=augment, visualize=visualize).unsqueeze(0)), dim=0)
-                pred = [pred, None]
-            else:
-                pred = model(im, augment=augment, visualize=visualize)
-        # NMS
-        with dt[2]:
-            pred = non_max_suppression(pred, conf_thres, iou_thres, classes, agnostic_nms, max_det=max_det)
-
-        # Second-stage classifier (optional)
-        # pred = utils.general.apply_classifier(pred, classifier_model, im, im0s)
-
-        for i, det in enumerate(pred):  # per image
-            seen += 1
-           
-            p, im0, frame = path[i], im0s[i].copy(), dataset.count
-            s += f"{i}: "
-    
-
-            p = Path(p)  # to Path
-            save_path = str(save_dir / p.name)  # im.jpg
-            txt_path = str(save_dir / "labels" / p.stem) + ("" if dataset.mode == "image" else f"_{frame}")  # im.txt
-            s += "%gx%g " % im.shape[2:]  # print string
-            gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
-            imc = im0.copy() if save_crop else im0  # for save_crop
-            annotator = Annotator(im0, line_width=line_thickness, example=str(names))
-            if len(det):
-                # Rescale boxes from img_size to im0 size
-                det[:, :4] = scale_boxes(im.shape[2:], det[:, :4], im0.shape).round()
-
-                # Print results
-                for c in det[:, 5].unique():
-                    n = (det[:, 5] == c).sum()  # detections per class
-                    s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string
-
-                # Write results
-                for *xyxy, conf, cls in reversed(det):
-                    c = int(cls)  # integer class
-                    label = names[c] if hide_conf else f"{names[c]}"
-                    confidence = float(conf)
-                    confidence_str = f"{confidence:.2f}"
-
-                    if save_txt:  # Write to file
-                        xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
-                        line = (cls, *xywh, conf) if save_conf else (cls, *xywh)  # label format
-                        with open(f"{txt_path}.txt", "a") as f:
-                            f.write(("%g " * len(line)).rstrip() % line + "\n")
-
-                    if view_img:  # Add bbox to image
-                        c = int(cls)  # integer class
-                        label = None if hide_labels else (names[c] if hide_conf else f"{names[c]} {conf:.2f}")
-                        annotator.box_label(xyxy, label, color=colors(c, True))
-                    # if save_crop:
-                        save_one_box(xyxy, imc, file=save_dir / "crops" / names[c] / f"{p.stem}.jpg", BGR=True)
-
-            # Stream results
-            im0 = annotator.result()
-            if view_img:
-                if platform.system() == "Linux" and p not in windows:
-                    windows.append(p)
-                    cv2.namedWindow(str(p), cv2.WINDOW_NORMAL | cv2.WINDOW_KEEPRATIO)  # allow window resize (Linux)
-                    cv2.resizeWindow(str(p), im0.shape[1], im0.shape[0])
-                cv2.imshow(str(p), im0)
-                cv2.waitKey(1)  # 1 millisecond
-
-            # Save results (image with detections)
-            if False:
-                if dataset.mode == "image":
-                    cv2.imwrite(save_path, im0)
-                else:  # 'video' or 'stream'
-                    if vid_path[i] != save_path:  # new video
-                        vid_path[i] = save_path
-                        if isinstance(vid_writer[i], cv2.VideoWriter):
-                            vid_writer[i].release()  # release previous video writer
-                        if vid_cap:  # video
-                            fps = vid_cap.get(cv2.CAP_PROP_FPS)
-                            w = int(vid_cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-                            h = int(vid_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-                        else:  # stream
-                            fps, w, h = 30, im0.shape[1], im0.shape[0]
-                        save_path = str(Path(save_path).with_suffix(".mp4"))  # force *.mp4 suffix on results videos
-                        vid_writer[i] = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*"mp4v"), fps, (w, h))
-                    vid_writer[i].write(im0)
-
-        # Print time (inference-only)
-        LOGGER.info(f"{s}{'' if len(det) else '(no detections), '}{dt[1].dt * 1E3:.1f}ms")
-
-    # Print results
-    t = tuple(x.t / seen * 1e3 for x in dt)  # speeds per image
-    LOGGER.info(f"Speed: %.1fms pre-process, %.1fms inference, %.1fms NMS per image at shape {(1, 3, *imgsz)}" % t)
-    if save_txt or save_img:
-        s = f"\n{len(list(save_dir.glob('labels/*.txt')))} labels saved to {save_dir / 'labels'}" if save_txt else ""
-        LOGGER.info(f"Results saved to {colorstr('bold', save_dir)}{s}")
-    if update:
-        strip_optimizer(weights[0])  # update model (to fix SourceChangeWarni
 
 async def mains():
     try:
@@ -420,12 +266,17 @@ async def mains():
         print("Error ")
         
 async def inference1():
-    asyncio.create_task(run(source="http://192.168.1.3:4747/video"))
+    global form
+    asyncio.create_task(run(idx=1,source="http://192.168.1.4:8080/?action=stream"))
     
     
 async def inference2():
-    pass
-    asyncio.create_task(run(source="http://192.168.1.3:4747/video"))
+    global form
+    asyncio.create_task(run(idx=2,source="http://192.168.1.4:8081/?action=stream"))
+    
+async def inference3():
+    global form
+    asyncio.create_task(run(idx=3,source="http://192.168.1.3:8080/?action=stream"))
 
 
 
@@ -437,11 +288,19 @@ async def inference2():
 def start1():
     asyncio.run(inference1())
 def start2():   
-    asyncio.run(inference1())
+    asyncio.run(inference2())
+def start3():   
+    asyncio.run(inference3())
    
+formThread = Thread(target=form.launchApp, args=(mqtt_test.mymqtt,))
 inf1 = Thread(target=start1)
 inf2 = Thread(target=start2)
+inf3 = Thread(target=start3)
+formThread.start()
 inf1.start()
 inf2.start()
+inf3.start()
+formThread.join()
 inf1.join()
 inf2.join(0)
+inf3.join(0)
