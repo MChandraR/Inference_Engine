3634c270bd866f24d6a2e99040272ac347032c5b
MChandraR <2201020103@student.umrah.ac.id>
2024-10-25 21:47:45 +0700

    Update AI

--- utils/folder.py
+++ utils/folder.py
@@ -1,6 +1,7 @@
 import os
 import cv2
 import pandas
+from datetime import datetime
 
 def create_folder_in_public(public_dir='public/captures'):
     # Jika direktori 'public' belum ada, buat terlebih dahulu
@@ -42,10 +43,21 @@ def get_image_path(folder_id, image_id):
     else:
         return None
 
+datas = []
+
 def save_to_excel(data):
-    df  = pandas.DataFrame(data, columns=["Time", "HDG", "SOG", "COG", "DAY", "DATE","Lat Dir", "Lat", "Lon Dir", "Lon"])
+    df  = pandas.DataFrame(data, columns=["DAY", "DATE", "TIME", "HDG", "SOG", "COG", "Lat Dir", "Lat", "Lon Dir", "Lon"])
     df.index += 1
     df.index.name = "No"
     df.to_excel("output.xlsx", sheet_name="Sheet 1", index=True)
     print("Berhasil mengambil data")
     return
+
+# times = datetime.now().strftime('%H:%M:%S.%f')[:-3]
+
+# datas.append([times,2,3,4,5,6,7,8,9,0])
+# datas.append([times,2,3,4,5,6,7,8,9,0])
+# datas.append([times,2,3,4,5,6,7,8,9,0])
+# datas.append([times,2,3,4,5,6,7,8,9,0])
+
+# save_to_excel(datas)
